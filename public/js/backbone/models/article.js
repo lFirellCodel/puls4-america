aefd51dc461d24dafc46bff1b03c3c52ff969a42
lFirellCodel <orkube_123@hotmail.com>
2015-06-01 17:43:10 -0500

    alll
    all

--- public/js/backbone/models/article.js
+++ public/js/backbone/models/article.js
@@ -1,3 +1,8 @@
 Puls4.Models.Article = Backbone.Model.extend({
 	url : '/articles/'
 });
+
+
+// este modelo que estoy crenado apunta al article.
+// en el init defino la variable locales 
+//net space
